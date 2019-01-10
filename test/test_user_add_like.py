f39bc75cfe8fbe8499013b88172f1a8535fbd299
Tatiana Lapitskaya <moskvina.tania.vit@gmail.com>
2019-01-10 13:57:25 +0700

    add tests

---
+++ test/test_user_add_like.py
@@ -0,0 +1,13 @@
+import time
+import pytest
+
+
+def test_add_comment(app):
+    app.open_home_page()
+    app.session.log_in(username="tmoskvina@sinergo.ru", password="Qq!123")
+    app.pp.open_pain_points_tab()
+    time.sleep(1)
+    #app.pp.put_like()
+    app.pp.add_comment()
+    time.sleep(1)
+    app.session.log_out()
